76612978d6fa5bd3cfc5a64b4d24749cea029cb3
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-15 20:20:07 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,6 @@
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
