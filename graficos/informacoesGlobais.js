5223ab9479b330f0e04fffb83d13453cd3a443c1
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-15 20:37:11 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url=':https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/educacao/educacao-dados-globais.json'
 
 
 async function vizualizarInformacoesGlobais() {
@@ -7,3 +7,4 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
 }
 vizualizarInformacoesGlobais()
+
