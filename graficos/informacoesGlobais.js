aa07032d86571a6436bbf7a7613156dd8db294cd
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-15 21:13:55 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,9 +2,14 @@ const url='https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicos
 
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados)
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+    console.log("Temos "+dados.total_pessoas_mundo+" pessoas mundo verdadeira chave para a transformação social.");
+    console.log("E "+dados.total_acesso_educacao+"  têm acesso à educação.");
+    console.log("Ficando em média "+dados+" de estudos por dia.");
+
 }
-vizualizarInformacoesGlobais()
+
+vizualizarInformacoesGlobais();
 
