d697556a7e60284872da23241a3d37551f278107
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 20:22:06 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,6 +5,15 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
     console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+
+
+
+
     //console.log("Temos "+dados.total_pessoas_mundo+" pessoas mundo verdadeira chave para a transformação social.");
     //console.log("E "+dados.total_acesso_educacao+"  têm acesso à educação.");
     //console.log("Ficando em média "+dados+" de estudos por dia.");
