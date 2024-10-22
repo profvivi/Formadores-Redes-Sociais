890ac64252a18a790687c4253aa387059595fd35
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 20:58:12 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -14,7 +14,14 @@ async function vizualizarInformacoesGlobais() {
             Ela envolve um desejo profundo por conhecimento e crescimento pessoal, refletindo a necessidade de um ambiente que valorize a formação integral. 
             À medida que a sociedade avança, o papel da educação se torna ainda mais crucial, não apenas para abrir portas, mas para cultivar um futuro onde todos possam encontrar seu lugar e contribuir de forma significativa. Assim, ao olharmos para o horizonte, 
             percebemos que o verdadeiro sucesso está enraizado na educação que transforma`
-    console.log(paragrafo)
+   
+
+   
+const container = document.getElementById(‘graficos-
+container’)
+container.appendChild(paragrafo)
+}
+
 
 
 
