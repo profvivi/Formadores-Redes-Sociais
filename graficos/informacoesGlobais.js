2665976dd4f5606a97186d95db5312988ce014b9
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-22 21:40:23 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/educacao/educacao-dados-globais.json'
+const url='https://raw.githubusercontent.com/profvivi/Formadores-Redes-Sociais/refs/heads/main/index.html'
 
 
 async function vizualizarInformacoesGlobais() {
@@ -10,10 +10,7 @@ async function vizualizarInformacoesGlobais() {
 
     paragrafo.innerHTML = `Em um mundo com ${dados.total_pessoas_mundo} bilhões de pessoas, a educação emerge como a verdadeira chave para a transformação social. 
             Sendo que ${dados.total_pessoas_com_acesso_a_educacao} de pessoas têm acesso à educação com tempo médio tempo de ${dados.tempo_medio_dia_estudando} horas de estudos por dia.
-            Contudo, a jornada educacional não se resume apenas a diplomas. 
-            Ela envolve um desejo profundo por conhecimento e crescimento pessoal, refletindo a necessidade de um ambiente que valorize a formação integral. 
-            À medida que a sociedade avança, o papel da educação se torna ainda mais crucial, não apenas para abrir portas, mas para cultivar um futuro onde todos possam encontrar seu lugar e contribuir de forma significativa. Assim, ao olharmos para o horizonte, 
-            percebemos que o verdadeiro sucesso está enraizado na educação que transforma`
+            
    
 
    
