1aacc29f08e82b2633f5a31a28333cda017299ee
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-23 00:35:00 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,10 +9,10 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.classList.add('graficos-container__texto')
 
     paragrafo.innerHTML = 
-        `Em um mundo com ${dados.total_pessoas_mundo} bilhões de pessoas, a educação emerge 
+        `Em um mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, a educação emerge 
         como a verdadeira chave para a transformação social. 
-        Sendo que ${dados.total_pessoas_com_acesso_a_educacao} de pessoas têm acesso à educação com tempo médio 
-        tempo de ${dados.tempo_medio_dia_estudando} horas de estudos por dia.`
+        Sendo que <span>${dados.total_pessoas_com_acesso_a_educacao}</span> de pessoas têm acesso à educação com tempo médio 
+        tempo de <span>${dados.tempo_medio_dia_estudando}</span> horas de estudos por dia.`
         console.log(paragrafo)
 
         const container = document.getElementById('graficos-container')
