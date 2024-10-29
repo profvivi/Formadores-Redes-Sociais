23863639b10da588e81f5a5a2ea225c7694d9d3d
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-29 15:24:22 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url='https://raw.githubusercontent.com/profvivi/Formadores-Redes-Sociais/refs/heads/main/dados.json';
+const url='https://raw.githubusercontent.com/profvivi/Formadores-Redes-Sociais/refs/heads/main/dados-globais.json';
 
 
 async function vizualizarInformacoesGlobais() {
