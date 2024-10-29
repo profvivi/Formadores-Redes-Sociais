8d4576fa7cd6582a2dbc8eaf6e8aae1cfb8bce09
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-29 18:34:02 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,12 +8,12 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 60)
     const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
-    const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
+    //const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
 
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span> bilhões?  O tempo médio gasto por dia estudando é de <span>${horas} horas </span> e <span>${minutos} minutos</span>  e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span> bilhões. Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação. <br>Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?` 
+    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span> bilhões?  O tempo médio gasto por dia estudando é de <span>${horas} horas </span> e <span>${minutos} minutos</span>  e o total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}</span> bilhões./* Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação*/. <br>Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?` 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
