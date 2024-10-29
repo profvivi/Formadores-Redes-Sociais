ff76d3265b53b6d210a24d66eee217bce68dfb15
Viviane <106716101+profvivi@users.noreply.github.com>
2024-10-29 15:45:31 -0300

    s

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,24 +1,19 @@
-const url='https://raw.githubusercontent.com/profvivi/Formadores-Redes-Sociais/refs/heads/main/dados-globais.json';
-
+const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    console.log(dados);
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
 
-    paragrafo.innerHTML = 
-        `Em um mundo com <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas, a educação emerge 
-        como a verdadeira chave para a transformação social. 
-        Sendo que <span>${dados.total_pessoas_com_acesso_a_educacao}</span> de pessoas têm acesso à educação com tempo médio 
-        tempo de <span>${dados.tempo_medio_dia_estudando}</span> horas de estudos por dia.`
-        console.log(paragrafo)
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+}
 
-        const container = document.getElementById('graficos-container')
-        container.appendChild(paragrafo)
-        }
-    
-        visualizarInformacoesGlobais()  //invocar a função
-        */
+vizualizarInformacoesGlobais()
+
+        
 
